b65ef5314ef63562184425376b712f2d004c969d
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-09 20:43:43 -0300

    Modify displayBooks() to loop the library array

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dialog = document.querySelector("dialog");
 const inputs = document.querySelectorAll("input, textarea");
 const cancelButton = document.querySelector("#cancel");
 const bookInfo = document.querySelector("form");
+const booksContainer = document.querySelector("#books-container");
 
 function Book(title, author, pages, notes) {
     this.title = title;
@@ -12,16 +13,22 @@ function Book(title, author, pages, notes) {
     this.notes = notes;
 }
 
-function displayBook() {
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    let newBook = myLibrary.at(-1);
-        for (key in newBook) {
+function displayBooks() {
+    booksContainer.replaceChildren();
+
+    myLibrary.forEach(book => {
+        const div = document.createElement("div");
+        div.classList.add("book-card");
+
+        for (key in book) {
             const p = document.createElement("p");
-            let info = `${key}`;
-            p.textContent = info.at(0).toUpperCase() + info.slice(1) + `: ${newBook[key]}`;
+            const formattedKey = key.At(0).toUpperCase() + key.slice(1);
+            p.textContent = `${formattedKey}: ${value}`;
             div.appendChild(p);
-    }
+        }
+
+        booksContainer.appendChild(div);
+    });
 }
 
 function addBookToLibrary(event) {
@@ -32,7 +39,7 @@ function addBookToLibrary(event) {
     event.preventDefault();
     dialog.close();
     bookInfo.reset();
-    displayBook();
+    displayBooks();
 }
 
 bookInfo.addEventListener("submit", addBookToLibrary);
