86c8c33c89a7e6cb0bbd4b48b0cd5990a088f9a6
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-09 16:12:28 -0300

    Clean the previously mentioned function

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const myLibrary = [];
-
 function Book(title, author, pages, notes) {
-
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.notes = notes;
-
-    this.info = function() {
-        return {
-            title: `${title}`,
-            author: `${author}`,
-            pages: `${pages}`,
-            notes: `${notes}`
-        }
-    };
 }
