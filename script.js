b36bff9f0a4ed285666fb889fa2a5fc2741f6f6e
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-09 16:14:55 -0300

    Create functions for adding the books to an array

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+const myLibrary = [];
+const newBookButton = document.querySelector("#add");
+const dialog = document.querySelector("dialog");
+const inputs = document.querySelectorAll("input, textarea");
+const cancelButton = document.querySelector("#cancel");
+const bookInfo = document.querySelector("form");
+
 function Book(title, author, pages, notes) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.notes = notes;
 }
+
+const inputValues = Array.from(inputs).map(input => input.value);
+
+function addBookToLibrary(event) {
+    const book = new Book(...inputValues);
+    myLibrary.push(book);
+    console.log(myLibrary);
+    event.preventDefault();
+    dialog.close();
+    bookInfo.reset();
+}
+
+bookInfo.addEventListener("submit", addBookToLibrary);
+
+newBookButton.addEventListener("click", () => {
+    dialog.showModal(); 
+});
+
+cancelButton.addEventListener("click", () => {
+    dialog.close(); 
+});
