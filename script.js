7f4c7bb0793350ffdbe1036f3e1a47dff807fa48
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-09-10 12:58:55 -0300

    Make code cleaner

--- script.js
+++ script.js
@@ -13,12 +13,8 @@ function Book(title, author, pages, notes) {
     this.notes = notes;
     this.status = "Not read";
 
-    toggleStatus = function() {
-        if (this.status === "Not read") {
-            this.status = "Read";
-        } else {
-            this.status = "Not read";
-        }
+    this.toggleStatus = function() {
+        this.status = this.status === "Not read" ? "Read" : "Not read";
         displayBooks();
     };
 }
@@ -28,63 +24,58 @@ function displayBooks() {
 
     myLibrary.forEach((book, index) => {
         const card = document.createElement("div");
-        const deleteButton = document.createElement("button");
-        const readButton = document.createElement("button");
-        
         card.classList.add("book-card");
 
+        for (key in book) {
+            if (key !== "toggleStatus") {
+            const p = document.createElement("p");
+            const capitalizedKey = key.at(0).toUpperCase() + key.slice(1);
+            p.textContent = `${capitalizedKey}: ${book[key]}`;
+            card.appendChild(p);
+            }
+        };
+
+        const deleteButton = document.createElement("button");
         deleteButton.classList.add("delete-button");
         deleteButton.setAttribute("type", "button");
-        deleteButton.setAttribute("data-index",`${index}`);
+        deleteButton.setAttribute("data-index", index);
         deleteButton.textContent = "Remove book";
+        card.appendChild(deleteButton);
 
+        const readButton = document.createElement("button");
         readButton.classList.add("read-button");
         readButton.setAttribute("type", "button");
+        readButton.setAttribute("data-index", index);
         readButton.textContent = "Change read status";
-
-        for (key in book) {
-            const p = document.createElement("p");
-            const formattedKey = key.at(0).toUpperCase() + key.slice(1);
-            p.textContent = `${formattedKey}: ${book[key]}`;
-            card.appendChild(p);
-        }
-
-        card.appendChild(deleteButton);
         card.appendChild(readButton);
+
         booksContainer.appendChild(card);
     });
 }
 
 function addBookToLibrary(event) {
-    const inputValues = Array.from(inputs).map(input => input.value);
-    const book = new Book(...inputValues);
-    myLibrary.push(book);
     event.preventDefault();
+    const inputValues = Array.from(inputs).map(input => input.value);
+    myLibrary.push(new Book(...inputValues));
     dialog.close();
     bookInfo.reset();
     displayBooks();
 }
 
-function deleteBook(event) {
-    if (event.target.classList.contains("delete-button")) {
-        const button = event.target;
-        const index = button.getAttribute("data-index");
+function handleButtonClick(event) {
+    const button = event.target;
+    const index = button.dataset.index;
+    if (button.classList.contains("delete-button")) {
         myLibrary.splice(index,1);
-        displayBooks();
-    } else if (event.target.classList.contains("read-button")) {
-        const button = event.target.previousElementSibling;
-        const index = button.getAttribute("data-index");
+    } else if (button.classList.contains("read-button")) {
         myLibrary[index].toggleStatus();
     }
+    displayBooks();
 }
 
 bookInfo.addEventListener("submit", addBookToLibrary);
-
-booksContainer.addEventListener("click", deleteBook);
-
-newBookButton.addEventListener("click", () => {
-    dialog.showModal(); 
-});
+booksContainer.addEventListener("click", handleButtonClick);
+newBookButton.addEventListener("click", () => dialog.showModal());
 
 cancelButton.addEventListener("click", () => {
     dialog.close();
